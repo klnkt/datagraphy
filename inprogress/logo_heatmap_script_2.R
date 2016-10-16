c8afe094e9e47dd4ae750fd569405440b203c7c3
Ekaterina Efremova <ame-team@yandex.ru>
2016-10-16 21:52:09 +0300

    initial commit

---
+++ inprogress/logo_heatmap_script_2.R
@@ -0,0 +1,3 @@
+logo_matrix <- data.matrix(logo.heatmap.2)
+pink_colors <- c("#ffffff", "#fffbfb", "#feebeb","#fedadb","#fdcaca","#fcb9ba","#fca9aa","#fb999a","#fb8889","#fa7879","#f96869","#f95759","#f84748","#f83638","#f72628")
+logo_heatmap <- heatmap(logo_matrix, Rowv=NA, Colv=NA,col=pink_colors, scale="none", margins=c(5,10))
